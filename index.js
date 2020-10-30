d9526016fcce9d491b135275fc905460e9f3b98d
anny_@DESKTOP-ACQR4TU <martins.stfn@gmail.com>
2020-10-30 12:28:24 -0300

    TEST

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ import cipher from './cipher.js';
 
 console.log(cipher);
 
-var cipher = ["A","B"];
+var cipher = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","X","W","Y","Z"];
+
+function cifra(string){
+    return
+}
