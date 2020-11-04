3892d8432f5cf9cc813cd93127b1a31681bc99f4
anny_@DESKTOP-ACQR4TU <martins.stfn@gmail.com>
2020-11-04 11:07:08 -0300

    TEST

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
-import cipher from './cipher.js';
+// import cipher from './cipher.js';
 
-console.log(cipher);
+//  console.log(cipher);
+
+// variavel para o botão Encrypt
+var crip = document.getElementById ("crip");
+crip.addEventListener("click", cipher);
 
 
+// var decrip = document.getElementById("decrip");
+// decrip.addEventListener("click", decipher);
 
-function cifra(string){
-    return
-}
+
+let tamDoAlfabeto = 26;
+let codeASC = 65;
+let novaMensagem = "";
+
+
+  function cipher (evt) {
+    evt.preventDefault(); 
+    
+    let myText = document.getElementById ("myText").value;
+    let offset = Number(document.getElementById("choose").value);
+
+    for (let i = 0; i < myText.length; i++){
+        
+        let codigo = ((myText.charCodeAt(i)- codeASC + offset) % tamDoAlfabeto) + codeASC;
+        novaMensagem += String.fromCharCode(codigo)
+        console.log(novaMensagem)
+        
+        // document.getElementById("myText").innerHTML = "";
+        
+    }
+   
+} 
+ 
