ad8c6e27fb1ab2614a01f7084eeaf98063c0ceed
anny_@DESKTOP-ACQR4TU <martins.stfn@gmail.com>
2020-11-06 20:23:58 -0300

    TEST

--- cipher.js
+++ cipher.js
@@ -1,6 +1,44 @@
+
 const cipher = {
-  // ...
-};
+  encode(offset, myText) {
+    var tamDoAlfabeto = 26;
+    var codeASC = 65;
+    let resultCrip = "".toUpperCase();
+    
 
+    for (var i = 0; i < myText.length; i++) {
 
+      var codigoCaractere = myText.charCodeAt(i);
+      if (codigoCaractere >= 65 && codigoCaractere <= 90) {
+        let codigo = ((codigoCaractere - codeASC + offset) % tamDoAlfabeto) + codeASC;
+        resultCrip += String.fromCharCode(codigo);
+      } else {
+        resultCrip += String.fromCharCode(codigoCaractere);
+      
+      }
+
+    }
+      return resultCrip
+      // console.log(resultCrip)
+  },
+
+  decode(offset, myText) {
+    var tamDoAlfabeto = 26;
+    var codeASC = 65;
+    let resultDecrip = "".toUpperCase();
+
+    for (let i = 0; i < myText.length; i++) {
+      let codigoCaractere = myText.charCodeAt(i);
+      if (codigoCaractere >= 65 && codigoCaractere <= 90) {
+        let codigo2 = ((codigoCaractere + codeASC - offset) % tamDoAlfabeto) + codeASC;
+        resultDecrip += String.fromCharCode(codigo2);
+      } else {
+        resultDecrip += String.fromCharCode(codigoCaractere);
+
+      }
+
+    }
+    return resultDecrip
+  },
+}
 export default cipher;
--- index.js
+++ index.js
@@ -1,56 +1,32 @@
 import cipher from './cipher.js';
+//  console.log(cipher);
 
- console.log(cipher);
-
-
-const tamDoAlfabeto = 26;
-const codeASC = 65;
-
-// variavel para o botão Encrypt
 var crip = document.getElementById ("crip");
 crip.addEventListener("click", criptar);
 
-  function criptar (evt) {
-    evt.preventDefault(); 
-  
-    let myText = document.getElementById("myText").value.toUpperCase();
-    let offset = Number(document.getElementById("choose").value);
-    let mensagemCrip = "".toUpperCase();
-    mensagemCrip;
-    
-    
-    for (let i = 0; i < myText.length; i++){
-      let codigoCaractere = myText.charCodeAt(i);
-      let codigo = ((codigoCaractere - codeASC + offset) % tamDoAlfabeto) + codeASC;
-      mensagemCrip += String.fromCharCode(codigo);
-      codigo;
-      
-      
-      // document.getElementById("myText").innerHTML = "";
-      
-    }
-    alert(mensagemCrip)
- 
-  }
+var resultCrip = document.getElementById("result").innerHTML;
+var resultDecrip = document.getElementById("result").innerHTML;
 
 
+
+function criptar (evt) {
+    evt.preventDefault();
+    let myText = document.getElementById("myText").value;
+    let myText2 = myText.toString().toUpperCase();
+    let offset = Number(document.getElementById("choose").value);    
+      
+    document.getElementById("result").innerHTML = cipher.encode(offset, myText2)
+}
 
 var decrip = document.getElementById("decrip");
 decrip.addEventListener("click", descriptar);
 
-  function descriptar (evt){
-    evt.preventDefault();
-    let myText = document.getElementById("myText").value.toUpperCase();
+function descriptar (evt){
+    evt.preventDefault()
+    let myText = document.getElementById("myText").value;
+    let myText2 = myText.toString().toUpperCase();
     let offset = Number(document.getElementById("choose").value);
-    let mensagemDecrip = "".toUpperCase();
-    mensagemDecrip;
 
-    for (let i = 0; i < myText.length; i++){
-      let codigoCaractere = myText.charCodeAt(i);
-      let codigo2 = ((codigoCaractere + codeASC - offset) % tamDoAlfabeto) + codeASC;
-      mensagemDecrip += String.fromCharCode(codigo2);
-      codigo2;
 
-    }
-    alert(mensagemDecrip)
-  }
+    document.getElementById("result").innerHTML = cipher.decode(offset, myText2)
+}
